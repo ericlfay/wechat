6c234a973780d7730d85efba1e97bb2016f46317
ericlfay <18708116154@163.com>
2018-09-11 17:12:54 +0800

    提交

---
+++ pages/soccer_sign/soccer_sign.js
@@ -0,0 +1,123 @@
+// pages/soccer_sign/soccer_sign.js
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    username: '',
+    realname: '',
+    position_index: 0,
+    date: '1970-01-01',
+    position: '请选择长踢位置',
+  },
+  bindDateChange: function(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindPickerChange: function(e) {
+    this.setData({
+      position_index: e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  usernameInput: function(e) {
+    this.setData({
+      username: e.detail.value
+    })
+  },
+  realnameInput: function(e){
+    this.setData({
+      realname: e.detail.value
+    })
+  },
+  onLoad: function(options) {
+
+  },
+  submit: function(e) {
+    console.log(this.data.position_list[this.data.position_index][0])
+    wx.request({
+      url: 'https://ericlfay.cn:8080/wechat/user/',
+      method: "POST",
+      data:{
+        username: this.data.username,
+        wechat_username: app.globalData.userInfo.nickName,
+        realname: this.data.realname,
+        birth: this.data.date,
+        user_tag: '0',
+        position: this.data.position_list[this.data.position_index][0]
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function(res) {
+        console.log(res.data)
+      }
+    })
+  },
+/**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+onReady: function() {
+  var _this = this
+  wx.request({
+    url: 'https://ericlfay.cn:8080/wechat/user/',
+    header: {
+      'content-type': 'application/json' // 默认值
+    },
+    success: function(res) {
+      _this.setData({
+        position_list: res.data.result,
+        real_name: res.data.realname
+      });
+    }
+  })
+},
+
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow: function() {
+
+},
+
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide: function() {
+
+},
+
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload: function() {
+
+},
+
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh: function() {
+
+},
+
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom: function() {
+
+},
+
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage: function() {
+
+}
+})
